299dba3ad6b416c3d8ef9552f9ad1c95bdbe0ea0
Kevin <quebin31@gmail.com>
2019-06-16 23:08:54 -0500

    Export modules as objects

---
+++ crydi/__init__.py
@@ -0,0 +1,13 @@
+import crydi.common
+import crydi.md4
+import crydi.md5
+import crydi.sha1
+import crydi.sha256
+import crydi.hmac
+
+common = crydi.common
+md4 = crydi.md4
+md5 = crydi.md5
+sha1 = crydi.sha1
+sha256 = crydi.sha256
+hmac = crydi.hmac
