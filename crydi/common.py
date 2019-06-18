964bfaaa6fc54a111e8418d80d353641d08c73b2
Kevin <quebin31@gmail.com>
2019-06-17 22:21:20 -0500

    Working with bytes

--- crydi/common.py
+++ crydi/common.py
@@ -1,140 +1,173 @@
-import re
+import struct
 import numpy as np
+from enum import Enum, unique
+
+# =================================================================================
+# Auxiliar variables
+# =================================================================================
+NUM_TYPE = {
+    1: np.uint8,
+    2: np.uint16,
+    4: np.uint32,
+    8: np.uint64,
+}
+
+@unique
+class ByteFormat(Enum):
+    BigEndian    = 0
+    LittleEndian = 1
+# =================================================================================
 
 # =================================================================================
 # Operations on words
 # =================================================================================
 def shift_left(x, n):
-    return np.uint32(x << n)
+    num_type = type(x)
+    return num_type(x << num_type(n))
 
 def shift_right(x, n):
-    return np.uint32(x >> n)
+    num_type = type(x)
+    return num_type(x >> num_type(n))
 
 def rotate_left(x, n):
-    return np.uint32(x << n) | np.uint32(x >> (32 - n))
+    num_type = type(x)
+    return num_type(shift_left(x, n) | shift_right(x, 32 - n))
 
 def rotate_right(x, n):
-    return np.uint32(x >> n) | np.uint32(x << (32 - n))
+    num_type = type(x)
+    return num_type(shift_right(x, n) | shift_left(x, 32 - n))
+
+def little_endian(x):
+    num_type = type(x)
+    x = struct.pack('>I', x)
+    x = int.from_bytes(x, 'little')
+    return num_type(x)
+# =================================================================================
 
 # =================================================================================
-# data is in hex format, size is the size of each block in 4-bit words (hex digit),
-# blocks is going to be an array with 32-bit words (8 hex digits) at least for its
-# first n - 1 blocks, the last one can have less than 32 bits
+# Utility functions for words
 # =================================================================================
-def segmentize_data(data, block_size=8):
-    block  = ''
-    blocks = []
+def valid_word_size(word_size):
+    one_count = 0
+    while word_size != 0:
+        one_count += (word_size & 1)
+        word_size >>= 1
 
-    for hex_digit in data:
-        block += hex_digit
-        if len(block) % block_size == 0:
-            blocks.append(block)
-            block = ''
-
-    # Don't append an empty word
-    if len(block) != 0:
-        blocks.append(block)
-
-    return blocks
+    return one_count == 1
+# =================================================================================
 
 # =================================================================================
-# Receive an array of 32-bit words (@blocks) with maybe the last one less than 32 
-# bits, and creates the padding so the total length is congruent to 448 mod 512
+# WordsArray store words of word_size, maybe last word is less than word_size
 # =================================================================================
-def perform_padding(blocks):
-    # Get the latest block length, if blocks is empty, then it's zero
-    try:
-        last_block_len = len(blocks[-1]) 
-    except Exception:
-        last_block_len = 0
+class WordsArray:
+    def __init__(self, word_size):
+        self.word_size   = word_size
+        self.words_array = []
 
-    # Append 1 to the last block and complete with 0 until it's length is 32 bits
-    if last_block_len % 8 != 0:
-        # It's an incomplete block (i.e. less than 32 bits)
-        blocks[-1] += '8' + '0' * (7 - last_block_len)
+    def __len__(self):
+        return len(self.words_array)
+
+    def __repr__(self):
+        return self.words_array.__repr__()
+
+    def __getitem__(self, index):
+        return self.words_array[index]
+    
+    def __setitem__(self, index, value):
+        self.words_array[index] = value
+
+    def append(self, word, byte_format=ByteFormat.BigEndian):
+        if byte_format == ByteFormat.LittleEndian:
+            word = little_endian(word)
+
+        self.words_array.append(word)
+# =================================================================================
+
+
+# =================================================================================
+# Encode string into bytes using an encoding (default: utf-8)
+# =================================================================================
+def encode(input_str, encoding='utf-8'):
+    bytes_array = input_str.encode(encoding)
+    return [np.uint8(byte) for byte in bytes_array]
+# =================================================================================
+
+# =================================================================================
+# Conver to bytes from hex string
+# =================================================================================
+def fromhex(hex_str):
+    bytes_array = bytes.fromhex(hex_str)
+    return [np.uint8(byte) for byte in bytes_array]
+# =================================================================================
+
+# =================================================================================
+# Receive an array of bytes, and creates the padding so the total length 
+# is congruent to rule_size[0] mod rule_size[1] (in bytes)
+# =================================================================================
+def perform_padding(bytes_array, start_byte=0x80, fill_byte=0x00, rule_size=(56, 64)):
+    bytes_array.append(np.uint8(start_byte))
+
+    while len(bytes_array) % rule_size[1] != rule_size[0]:
+        bytes_array.append(np.uint8(fill_byte))
+
+    return bytes_array
+# =================================================================================
+
+# =================================================================================
+# Transform bytes into words of word_size (in bytes)
+# =================================================================================
+def bytes_to_words(bytes_array, word_size=4, byte_format=ByteFormat.BigEndian):
+    if not valid_word_size(word_size) or word_size > 8:
+        raise RuntimeError(f'Too big or incorrect word size ({word_size})')
+
+    if len(bytes_array) % word_size != 0:
+        raise RuntimeError(f'Word size with current number of bytes_array')
+
+    num_type = NUM_TYPE[word_size]
+    size  = 0
+    word  = None
+    words_array = WordsArray(word_size = word_size)
+
+    for byte in bytes_array:
+        word  = (word or num_type(0)) | num_type(byte)
+        size += 1
+        if size % word_size == 0:
+            words_array.append(word, byte_format)
+            size = 0
+            word = None
+        else:
+            word = word << num_type(8)
+
+    return words_array
+# =================================================================================
+
+# =================================================================================
+# Append the length to the words
+# =================================================================================
+def append_length(words_array, length, byte_format=ByteFormat.BigEndian):
+    length = int(length).to_bytes(8, 'big')
+    length = bytes_to_words(length, word_size=4, byte_format=ByteFormat.BigEndian)
+
+    if byte_format == ByteFormat.LittleEndian:
+        words_array.append(length[1])
+        words_array.append(length[0])
     else:
-        # The last block is complete, just append another one then
-        blocks.append('8' + '0' * 7)
+        words_array.append(length[0])
+        words_array.append(length[1])
 
-    # Fill with zeros until it's congruent with 448 mod 512 (bits)
-    # As len(blocks) give us the number of blocks of 32 bits 
-    # 448 mod 512 (bits) is equal to 14 mod 16 (32bits)
-    while (len(blocks) % 16) != 14:
-        blocks.append('0' * 8)
-
-    return blocks
-
+    return words_array
 # =================================================================================
-# Append input_length to the current blocks
-# =================================================================================
-def append_length(blocks, input_length, little_endian):
-    # Expand length to fill 64 bits
-    input_length = '0' * (16 - len(input_length)) + input_length
-    input_length = segmentize_data(input_length)
-
-    # Append the length
-    if little_endian:
-        blocks.append(input_length[1])
-        blocks.append(input_length[0])
-    else:
-        blocks.append(input_length[0])
-        blocks.append(input_length[1])
-
-    return blocks
-
-# =================================================================================
-# Transform each word in blocks to little endian
-# =================================================================================
-def big_to_little(blocks): 
-    def transform(block):
-        temp = ''.join(block[i: i + 2] for i in range(len(block) - 2, -1, -2))
-        return temp
-
-    for (index, block) in enumerate(blocks):
-        blocks[index] = transform(block)
-
-    return blocks
-
-# =================================================================================
-# Self-explaining, ensure it's np.uint32
-# =================================================================================
-def hex_to_uint32(blocks):
-    for (index, block) in enumerate(blocks):
-        blocks[index] = np.uint32(int(block, 16))
-    return blocks
-
-# =================================================================================
-# Is it a valid hex string?
-# =================================================================================
-def valid_hex(input_data):
-    match = re.match('^([0-9]|[a-f]|[A-F])*$', input_data)
-    return not (match is None)
-
-def utf8_to_hex(input_data):
-    return ''.join('{:x}'.format(ord(c)) for c in input_data)
-
 
 # =================================================================================
 # Perform padding, append length and transform to little if neccessary
 # =================================================================================
-def prepare_data(input_data, hex_input, little_endian):
-    # Convert input_data to hex format
-    if not hex_input:
-        input_data = utf8_to_hex(input_data)
-    else:
-        if not valid_hex(input_data):
-            raise RuntimeError('Invalid HEX')
+def prepare_data(input_data, hex_input, word_size, byte_format, encoding='utf-8'):
+    input_data   = fromhex(input_data) if hex_input else encode(input_data, encoding)
+    input_length = len(input_data) * 8
 
-    input_length = f'{np.uint64(len(input_data) * 4):x}'
+    input_data = perform_padding(input_data, start_byte=0x80, fill_byte=0x00, rule_size=(56, 64))
+    input_data = bytes_to_words(input_data, word_size, byte_format)
+    input_data = append_length(input_data, input_length, byte_format)
 
-    # Segmentize data and create padding 
-    input_blocks = segmentize_data(input_data)
-    input_blocks = perform_padding(input_blocks)
-
-    if little_endian:
-        input_blocks = big_to_little(input_blocks)
-
-    input_blocks = append_length(input_blocks, input_length, little_endian)
-    input_blocks = hex_to_uint32(input_blocks)
-
-    return input_blocks
+    return input_data
+# =================================================================================
